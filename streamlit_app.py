68b2f47aaed2e5a068e456431e82b9ab1b702410
trstacey14 <135732224+trstacey14@users.noreply.github.com>
2024-03-02 11:45:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-pip install streamlit langchain openai
-
-
-# In[2]:
-
-
 import streamlit as st
 from langchain.llms import OpenAI
-
-
-# In[3]:
-
-
 st.image('DSU_Logo.png', width=100)
 st.title('🦜🔗 Quickstart App')
 
-
-# In[4]:
-
-
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
-
-
-# In[5]:
-
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
-
-
-# In[6]:
-
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
@@ -45,10 +16,5 @@ with st.form('my_form'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
-
-
-# In[ ]:
-
-
 
 
